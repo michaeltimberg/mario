dbaf2ff85b5fb9094d898c1671aaf6adfc839d4c
Michael Timberg <timberg.1@osu.edu>
2016-04-15 15:23:12 -0400

    Third Commit!
    Changed all images to .gif to remove all whitespace.  Updated index.html
    and added to app.js so that Mario can jump onto and fall off of that
    first pipe.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,45 +31,108 @@ var main = function () {
 
         $('#mario').animate({
            bottom: "-=95px"
-         }, 100);
-      } else if (counterH === 20 || counterH === 29) {
-        $('#mario').removeClass('mirror');
+         }, 200);
+      } else if (counterH === 20 || counterH === 25 || counterH === 29 || counterH === 43 || counterH === 47 || counterH === 53) {
+        if ($('#mario').hasClass('mirror')) {
+          if (counterH === 53) {
+            $('#mario').animate({
+               bottom: "+=195px"
+             }, 100);
 
-        $('#mario').animate({
-           bottom: "+=195px"
-         }, 100);
+            $('#mario').animate({
+               left: "-=20px"
+             }, 50);
 
-        $('#mario').animate({
-           left: "+=20px"
-         }, 50);
+            $('#mario').animate({
+               bottom: "-=107px"
+             }, 200);
+          } else {
+            $('#mario').animate({
+               bottom: "+=195px"
+             }, 100);
 
-        $('#mario').animate({
-           bottom: "-=18px"
-         }, 100);
+            $('#mario').animate({
+               left: "-=20px"
+             }, 50);
 
-        counterH ++;
-        counterV ++;
+            $('#mario').animate({
+               bottom: "-=18px"
+             }, 200);
+          };
+
+          counterH --;
+        } else {
+          if (counterH === 47) {
+            $('#mario').removeClass('mirror');
+
+            $('#mario').animate({
+               bottom: "+=195px"
+             }, 100);
+
+            $('#mario').animate({
+               left: "+=20px"
+             }, 50);
+
+            $('#mario').animate({
+               bottom: "-=107px"
+             }, 200);
+          } else {
+            $('#mario').removeClass('mirror');
+
+            $('#mario').animate({
+               bottom: "+=195px"
+             }, 100);
+
+            $('#mario').animate({
+               left: "+=20px"
+             }, 50);
+
+            $('#mario').animate({
+               bottom: "-=18px"
+             }, 200);
+          };
+
+          counterH ++;
+        };
+
+          counterV ++;
       } else {
-        $('#mario').removeClass('mirror');
+        if ($('#mario').hasClass('mirror')) {
+          $('#mario').animate({
+             bottom: "+=195px"
+           }, 100);
 
-        $('#mario').animate({
-           bottom: "+=195px"
-         }, 100);
+          $('#mario').animate({
+             left: "-=20px"
+           }, 50);
 
-        $('#mario').animate({
-           left: "+=20px"
-         }, 50);
+          $('#mario').animate({
+             bottom: "-=195px"
+           }, 200);
 
-        $('#mario').animate({
-           bottom: "-=195px"
-         }, 100);
+          counterH --;
+        } else {
+          $('#mario').removeClass('mirror');
 
-        counterH ++;
+          $('#mario').animate({
+             bottom: "+=195px"
+           }, 100);
+
+          $('#mario').animate({
+             left: "+=20px"
+           }, 50);
+
+          $('#mario').animate({
+             bottom: "-=195px"
+           }, 100);
+
+          counterH ++;
+        };
       };
     } else if (keyCode == 37) {
       $('#mario').addClass('mirror');
 
-      if (counterH > 0 && ((counterH <= 46 && counterV === 0) || (counterH >= 55 && counterV === 0) || counterV === 1)) {
+      if (counterH > 0 && ((counterH <= 47 && counterV === 0) || (counterH >= 54 && counterV === 0) || counterV === 1)) {
         $('#mario').animate({
            left: "-=20px"
          }, 50);
@@ -79,7 +142,7 @@ var main = function () {
     } else if (keyCode == 39) {
       $('#mario').removeClass('mirror');
 
-      if (counterH < 62 && ((counterH <= 45 && counterV === 0) || (counterH >= 55 && counterV === 0) || counterV === 1) ) {
+      if (counterH < 68 && ((counterH <= 46 && counterV === 0) || (counterH >= 53 && counterV === 0) || counterV === 1) ) {
         $('#mario').animate({
            left: "+=20px"
          }, 50);
@@ -88,10 +151,18 @@ var main = function () {
       };
     };
 
-    if (((counterH >= 0) && (counterH <= 20)) || ((counterH >= 25) && (counterH <= 29)) || ((counterH >= 43) && (counterH <= 62))) {
+    if (((counterH >= 0) && (counterH <= 20)) || ((counterH >= 25) && (counterH <= 29)) || ((counterH >= 43) && (counterH <= 46)) || ((counterH >= 54) && (counterH <= 68))) {
       if (counterV === 1) {
         $('#mario').animate({
            bottom: "-=177px"
+         }, 200);
+
+        counterV--;
+      };
+    } else if (counterH === 47 || counterH === 53) {
+      if (counterV === 1) {
+        $('#mario').animate({
+           bottom: "-=88px"
          }, 200);
 
         counterV--;
